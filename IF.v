d03fadf78f9067269ac2b45842597bbcef6480b9
ELBradley <ebradle4@u.rochester.edu>
2013-10-20 16:19:47 -0400

    Single Fetch Removed.

--- IF.v
+++ IF.v
@@ -5,17 +5,16 @@ module IF(	CLK,
 		RESET, 
 		PCA_PR,
 		CIA_PR,
-		single_fetch,
 		taken_branch1,
 		taken_branch2, 
 		nextInstruction_address, 
 		PC_init, 
-		Instr1_fIM, 
+		Instr1_fIM, //From instruction Memory
 		Instr1_PR, 
 		Instr_address_2IM,
 		Instr2_PR,
 		FREEZE,
-		fetchNull1,
+		fetchNull1, 
 		no_new_fetch
 		);
 
@@ -28,48 +27,48 @@ module IF(	CLK,
   input	  [31: 0] nextInstruction_address;
   input	  [31: 0] PC_init;
   input	  [31: 0] Instr1_fIM;
-  input		  single_fetch;
   input		  CLK;
   input		  RESET;
   input		  FREEZE;
   input		  taken_branch1;
   input		  taken_branch2; 
   input		  no_new_fetch;
-  input			 fetchNull1;
+  input		fetchNull1;
 
   wire	   [31: 0] Instr1;
   wire	   [31: 0] Instr2;
-  wire	   [31: 0] PCA;
-  wire	   [31: 0] CIA;
+  wire	   [31: 0] PCA; //Program Counter with 4 added
+  wire	   [31: 0] CIA; //Current Instruction Address
 
   reg	    [31: 0] PC;
-  reg	    [31: 0] FPC;
+  reg	    [31: 0] FPC; //Fetch PC
 
-  assign Instr_address_2IM   = (taken_branch1|taken_branch2)? nextInstruction_address: PC;
-  assign PCA		 = (single_fetch)?PC:Instr_address_2IM+32'h00000008;
-  assign CIA		 = (single_fetch)?FPC:Instr_address_2IM;
-  assign Instr1	      = (fetchNull1)? 32'h00000000: ((single_fetch)? Instr2_PR : Instr1_fIM);
+  assign Instr_address_2IM   = (taken_branch1|taken_branch2)? nextInstruction_address: PC; //Chooses a next instuction based on if a branch is taken or not. 
+  assign PCA		 = PC;
+  assign CIA		 = FPC; 
+  assign Instr1	      = (fetchNull1)? 32'h00000000: (Instr2_PR);//if fetchcNUll, fetch a null instrcution, else: on single_fetch true get
+  //instruction from primary register and on single_fetch false, get from instruction memory. 
   
 
   // Pipeline Register (IF/ID)
   always @ (posedge CLK or negedge RESET)
   begin
-    if(!RESET)
+    if(!RESET) //Resets on RESET = 0.
       begin
 	Instr1_PR	    <= 32'b0;
 	Instr2_PR	    <= 32'b0;
 	PCA_PR	       <= 32'b0;
 	CIA_PR	       <= 32'b0;
 	FPC		  <= 32'b0;
-	PC		   <= PC_init;
+	PC		   <= PC_init; //Sets an initial program counter if program is reset
       end
-    else if(!no_new_fetch && !FREEZE)
+    else if(!no_new_fetch && !FREEZE) //if we aren't fetching something new and are not frozen then...
       begin
 	Instr1_PR	    <= Instr1;
 	PCA_PR	       <= PCA;
 	CIA_PR	       <= CIA;
 	FPC		  <= Instr_address_2IM;
-	PC		   <= Instr_address_2IM + ((single_fetch)?32'h00000004:32'h00000008);	
+	PC		   <= Instr_address_2IM + (32'h00000004);	
       end
   end
 
