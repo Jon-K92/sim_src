b0782ec3adca99260f224867405a75c411c56eed
ELBradley <ebradle4@u.rochester.edu>
2013-10-21 13:43:24 -0400

    Compiles!!

--- MEM.v
+++ MEM.v
@@ -61,7 +61,7 @@ module MEM (	CLK,
    input                 MemRead1;
    input                 MemWrite1;
 
-   wire         [31: 0] data_read_aligned;
+   reg         [31: 0] data_read_aligned;
    wire         [31: 0] Dest_Value;
    wire         [31: 0] aluResult;
    wire         [31: 0] readDataB;
@@ -75,18 +75,19 @@ module MEM (	CLK,
 
    // TA: bypassing is missing here!
 
-   assign Instr_OUT = Instr1;
-   assign MemRead_2DM = MemRead1;
-   assign MemWrite_2DM = (MemWrite1);
-   assign select1_WB = (do_writeback1_WB&&(writeRegister1_WB==(writeRegister1)))/*&&(!ALUSrc1)/**/;
-   assign data_write_2DM = ((select1_WB)?writeData1_WB:(readDataB1));
-   assign data_address_2DM = aluResult1;
+ assign	ALU_control = ALU_control1;
+ assign	aluResult = aluResult1;
+  assign select1_WB = (do_writeback1_WB&&(writeRegister1_WB==(writeRegister1)))/*&&(!ALUSrc1)/**/;
    
    always
      begin
       //  data_read_aligned = writeData2_WB; (not sure what this does...but involves second instruction.
-	ALU_control = ALU_control1;
-	aluResult = aluResult1;
+   Instr_OUT = Instr1;
+   MemRead_2DM = MemRead1;
+   MemWrite_2DM = (MemWrite1);
+   data_write_2DM = ((select1_WB)?writeData1_WB:(readDataB1));
+   data_address_2DM = aluResult1;
+
 	
 	       case(ALU_control)
         		6'b101101: begin //LWX
