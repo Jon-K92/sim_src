147b8fc003c5b77f035ae9ddb3306979ef53cbe0
ELBradley <ebradle4@u.rochester.edu>
2013-10-23 16:00:11 -0400

    Update EXE.v

--- EXE.v
+++ EXE.v
@@ -1,48 +1,44 @@
 //-----------------------------------------
 //	   Execute Stage
 //-----------------------------------------
-module EXE(	
-    CLK, 
+module EXE(	CLK, 
 		RESET,
 		ALUSrc1_PR,
 		ALUSrc1,
 		Instr1,
-		
 		Instr1_PR,
 		Dest_Value1,
 		Dst1_PR,
 		readDataB1,
 		readDataB1_PR,
-		
 		aluResult1_OUT,
 		do_writeback1_WB,
 		writeRegister1_WB,
 		Data1_WB,
 		ALU_control1_PR,
-		
 		ALU_control1,		
 		Data1_MEM,
 		writeRegister1_MEM,
 		do_writeback1_MEM, 
 		do_writeback1_PR,
-		
 		do_writeback1_ID,
 		readRegisterA1,
 		readRegisterB1,
 		writeRegister1, 
 		writeRegister1_PR, 
-		
 		Instr1_10_6, 
+		Instr1_15_0,
 		MemRead1, 
 		MemWrite1, 
 		MemRead1_PR, 
 		MemWrite1_PR, 
-		
 		Operand_A1, 
 		Operand_B1, 
 		MemtoReg1, 
 		MemtoReg1_PR, 
-		aluResult1_PR);
+		aluResult1_PR,
+		Regbase
+		);
 
    output reg     [31: 0] aluResult1_PR;
    output reg     [31: 0] Dst1_PR;
@@ -71,6 +67,7 @@ module EXE(
    input	  [ 4: 0] readRegisterB1;
    input	  [ 4: 0] writeRegister1;
    input	  [ 4: 0] Instr1_10_6;
+   input   [ 15: 0] Instr1_15_0;
    input	  [ 4: 0] writeRegister1_WB;
    input		  ALUSrc1;
    input	 	  do_writeback1_MEM;
@@ -81,6 +78,7 @@ module EXE(
    input		  MemWrite1;
    input		  CLK;
    input		  RESET;
+   input   [31:0] Regbase;
 	
    wire	   [31: 0] aluResult1;
    wire	   [31: 0] OpA1;
@@ -90,6 +88,8 @@ module EXE(
    wire	    [31: 0] HI;
    wire	    [31: 0] LO;
 
+   assign OpA1 = (MemWrite1)? Regbase : Operand_A1;
+   assign OpB1 = (ALUSrc1)? Instr1_15_0: Operand_B1;
 
 	// TA: Forwarding is missing here !
 
@@ -110,8 +110,20 @@ module EXE(
 			Dst1_PR <= 32'b0;
 			Instr1_PR <= 32'b0;
 			ALUSrc1_PR <= 1'b0;
-		else
-			do_writeback1_PR <=do_writeback1_ID;
+		end
+	else 
+	  begin
+	    MemtoReg1_PR <= MemtoReg1;
+			MemRead1_PR <= MemRead1;
+			MemWrite1_PR <= MemWrite1;
+			aluResult1_PR <= aluResult1;
+			writeRegister1_PR <= writeRegister1;
+			do_writeback1_PR <= 0;
+			ALU_control1_PR <= ALU_control1;
+			readDataB1_PR <= readDataB1;
+			//Dst1_PR <= ;
+			Instr1_PR <= Instr1;
+			ALUSrc1_PR <= ALUSrc1;
 		end
 	end
 
